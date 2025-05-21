5a3501395670aa629844d8aeabc8d19553de461e
a210.alumno <tanialorences@gmail.com>
2025-05-21 20:43:11 +0200

    Commit Biblioteca.kt

---
+++ src/Libro.kt
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+public class Libro (val titulo: String, var autor: String, var anio: String) {
+
+    override fun toString(): String {
+        return "Libro(título= $titulo, autor= $autor, anio= $anio)"
+    }
+
+}
